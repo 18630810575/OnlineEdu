c0c8ff5add945bd98ad1010f11f25a3ce3491a1d
18630810575 <690425164@qq.com>
2017-03-27 17:30:59 +0800

    201703271730

---
+++ OnlineEdu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OnlineEdu
+//
+//  Created by 孙锐 on 2017/3/27.
+//  Copyright © 2017年 孙锐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OnlineEdu/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  OnlineEdu
+//
+//  Created by 孙锐 on 2017/3/27.
+//  Copyright © 2017年 孙锐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
