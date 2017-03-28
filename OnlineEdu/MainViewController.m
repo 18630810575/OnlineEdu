401191d25a06990c5131cd4af2af3742cbd5ac98
18630810575 <690425164@qq.com>
2017-03-28 16:08:53 +0800

    201703281608

--- OnlineEdu/MainViewController.m
+++ OnlineEdu/MainViewController.m
@@ -16,22 +16,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self getWebInfoDemo];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+-(void)getWebInfoDemo{
+
+    [WebAgent getUserInfoDemo:@"0000" success:^(id responseObject) {
+        NSData *data = [[NSData alloc]initWithData:responseObject];
+        NSDictionary *dict= [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+        
+        NSLog(@"0000");
+    } failure:^(NSError *error) {
+        NSLog(@"1111");
+    }];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
